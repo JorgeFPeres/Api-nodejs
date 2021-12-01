235694a3e9eea4087f53420c7cd0049625591140
jorgefperes <jorge-peres@live.com>
2021-12-01 12:36:16 -0300

    chore: basic configs

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import CreateCourseService from './CreateCourseService'
+
+export function createCourse(request: Request, response: Response) {
+  CreateCourseService.execute({
+    name: 'NodeJs',
+    duration: 10,
+    educator: 'Jorgin',
+  })
+
+  CreateCourseService.execute({
+    name: 'NodeJs',
+    educator: 'Jorgin',
+  })
+}
