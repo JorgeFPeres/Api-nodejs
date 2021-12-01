6462ba613c6e3c37df1bb7a16c14f00a16b3875c
jorgefperes <jorge-peres@live.com>
2021-12-01 12:50:46 -0300

    chore: add ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
-import express from "express";
+import express, { response } from "express"
 
-const app = express();
+const app = express()
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
-  return res.json({ name: "works" });
-});
+  return res.json({ name: "works" })
+})
 
-app.listen(3333, () => console.log("Server is running"));
+app.post("/courses", (req, res) => {
+  const { name } = req.body
+  return res.json({ name })
+})
+
+app.listen(3333, () => console.log("Server is running"))
