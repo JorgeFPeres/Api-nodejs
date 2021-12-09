0e1fbf641a6297e7ee39f9acf5b4039cde7f37b8
jorgefperes <jorge-peres@live.com>
2021-12-09 08:50:08 -0300

    feat: error middleware

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,28 @@
 import "reflect-metadata"
-import express from "express"
+import express, { NextFunction, Response, Request } from "express"
+import "express-async-errors"
 import "./shared/container"
 import swaggerUI from "swagger-ui-express"
 import { router } from "./routes"
 import swaggerFile from "./swagger.json"
 import "./database"
+import { AppError } from "./erros/AppError"
 
 const app = express()
 
 app.use(express.json())
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile))
 app.use(router)
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    })
+  }
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  })
+})
 
 app.listen(3333, () => console.log("Server is running"))
