6a4052e77dd0e2cf61e3d285d976298c9f1300f9
jorgefperes <jorge-peres@live.com>
2021-12-11 18:52:54 -0300

    feat: create car use case

---
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -0,0 +1,8 @@
+import { ICreateCarDTO } from "../dto/ICreateCatDTO"
+import { Car } from "../infra/typeorm/entities/Car"
+
+interface ICarsRepository {
+  create(data: ICreateCarDTO): Promise<Car>
+  findByLicensePlate(license_plate: string): Promise<Car>
+}
+export { ICarsRepository }
