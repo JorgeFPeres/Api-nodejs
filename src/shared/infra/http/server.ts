931184de1c5479af0881408981aa8254e455d0aa
jorgefperes <jorge-peres@live.com>
2021-12-15 19:34:46 -0300

    feat: create a admin user

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,9 +5,10 @@ import "../../container"
 import swaggerUI from "swagger-ui-express"
 import { router } from "./routes"
 import swaggerFile from "../../../swagger.json"
-import "../typeorm"
+import createConnection from "@shared/infra/typeorm"
 import { AppError } from "../../errors/AppError"
 
+createConnection()
 const app = express()
 
 app.use(express.json())
